089485826af1fed02017ea55eb2859d49b5f1f8f
garzarobm <garzarobm@gmail.com>
2023-02-21 09:25:14 -0600

    first set of javascript slides

---
+++ week3/LectureDay1/javascript.js
@@ -0,0 +1,56 @@
+
+
+//declaration
+let my_number; //let example
+//assigning value to z
+my_number = "number 5";
+my_number = "number 8"; //reassignment 
+
+console.log(my_number);
+
+//constant example 'const'
+const my_fave_number = 7; // this variable cannot be changed
+
+console.log(my_fave_number); // prints 7
+
+let x = 10 + my_fave_number; // we can use the const
+console.log(x); // prints 17
+
+x = 9; // error, this cannot be changed
+console.log(x);
+
+/*
+//This is a number variable
+let x = 5;
+
+//This is a string variable
+let y = "number 5";
+*/
+
+
+//strings 
+let a = "Hello, World!"; // <-- Had to get that one out of the way!
+let b ='This is JavaScript!'; // Can use single or double quotes
+let c = "7"; //includes numbers, but not read as numbers 
+let d = "It's Seven"; //To include quotation marks in a string, you must use a different quotation inside and outside
+let e = 'It\'s Seven'; //alternatively, you can use backslashes to ignore special characters in strings
+
+//numbers
+let a_num = 7; //integer
+let b_num = 7.000; //float or double 
+let c_num = 0.450; //float or double 
+
+//boolean
+let a_bool = true;
+let b_bool = false;
+
+
+//javascript loose typing example
+let g = 'abc';
+console.log(g);
+console.log(typeof(g));
+g = 1;
+console.log(g);
+console.log(typeof(g));
+
+	
