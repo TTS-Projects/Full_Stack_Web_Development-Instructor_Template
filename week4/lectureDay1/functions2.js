a38b65b7a460fffc597ade5aae80d41371f2a1b0
garzarobm <garzarobm@gmail.com>
2023-03-02 08:37:08 -0600

    update functions2 before starting on thursday day2

--- week4/lectureDay1/functions2.js
+++ week4/lectureDay1/functions2.js
@@ -1,15 +1,15 @@
-//Declare an add function
-function add(number1, number2) {
-	return number1 + number2;
-}
+// //Declare an add function
+// function add(a, b) {
+// 	return a + b ;
+// }
 
-//Declare a function that takes a function as an argument
-function doMath(operation, number1, number2) {
-	return operation(number1,number2);
-}
+// //Declare a function that takes a function as an argument
+// function doMath(operation, number1, number2) {
+// 	return operation + number1 + number2;
+// }
 
-//Pass a function into another.
-let sum = doMath(add, 1, 2);
+// //Pass a function into another.
+// let sum = doMath(add(1,2), 3, 4);
 
 // let students = [
 // 	{name: 'John Lennon', average: 90},
@@ -51,14 +51,27 @@ let sum = doMath(add, 1, 2);
 
 // fullNames === ["Cam Newton", "Ted Ginn", "Greg Olsen"]
 
-let students = [
-	{name: 'Cam Newton', assignmentsCompleted: 6},
-	{name: 'Ted Ginn', assignmentsCompleted: 10},
-	{name: 'Greg Olsen', assignmentsCompleted: 8}
-]
+// let students = [
+// 	{firstName: 'Cam', lastName: 'Newton'},
+// 	{firstName: 'Ted', lastName: 'Ginn'},
+// 	{firstName: 'Greg', lastName: 'Olsen'}
+// ]
 
-let totalAssignments = students.reduce(function(sum,current){
-	return sum + current.assignmentsCompleted;
-}, 0);
+// let fullNames = students.map(function(student){
+// 	return ( (student.firstName) + " "  + (student.lastName) );
+// })
 
-totalAssignments === 24;
+// console.log(fullNames === ["Cam Newton", "Ted Ginn", "Greg Olsen"])
+
+// let students = [
+// 	{name: 'Cam Newton', assignmentsCompleted: 6},
+// 	{name: 'Ted Ginn', assignmentsCompleted: 10},
+// 	{name: 'Greg Olsen', assignmentsCompleted: 8}
+// ]
+
+// let totalAssignments = students.reduce(function(sum,current){
+// 	return sum + current.assignmentsCompleted;
+// }, 0);
+
+// totalAssignments === 24;
+
