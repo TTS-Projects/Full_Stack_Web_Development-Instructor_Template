fe4b102ccd6e6cc018c97bfd1b6e1f6eedb2ec5a
garzarobm <garzarobm@gmail.com>
2023-05-31 11:00:59 -0500

    slides redone for javascript data structures.

---
+++ week3/LectureDay3/slides-redone/dataStructures.js
@@ -0,0 +1,6 @@
+
+let teachers = ['Assaf', 'Shane'];
+teachers.push('Zack'); //['Assaf', 'Shane', 'Zack']
+let teacher1 = teachers.pop(); //teacher1 == 'Zack', teachers == ['Assaf', 'Shane']
+teachers.unshift('Zack'); // ['Zack', Assaf', 'Shane']
+let teacher = teachers.shift(); //teacher == 'Zack', teachers = ['Assaf', Shane']
