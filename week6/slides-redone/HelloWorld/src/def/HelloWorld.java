ff7be77196f55f478b81212ac776ee5d6709e98f
Mike <garzarobm@gmail.com>
2024-04-09 14:34:16 -0500

    Add HelloWorld.java and module-info.java files

---
+++ week6/slides-redone/HelloWorld/src/def/HelloWorld.java
@@ -0,0 +1,35 @@
+package def;
+
+public class HelloWorld {
+
+
+public static void main(String[] args) {
+    System.out.println("Hello, World!"); 
+
+    long BigLong = 230L;          // long (l, L)  
+    float SomeFloat = 927.42f;    // float (f, F)  
+    double someDbl = 392.2d;      // double (d, D)  
+    double wishfulSalary = 123_000_100_325.0d; // stored/printed as 123000100325.0
+     
+
+}
+
+}
+
+
+/* 
+slides 12 need package information such as
+
+'package def;
+
+public class HelloWorld {
+
+
+public static void main(String[] args) {
+    System.out.println("Hello, World!"); 
+}
+
+}
+
+
+*/
